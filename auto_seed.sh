7821f50b452bcbe9bd4c874db5aeba25d284f9a4
Indrid Cold <102763446+Indr1d-C0ld@users.noreply.github.com>
2025-01-20 21:11:08 +0100

    Update auto_seed.sh

--- auto_seed.sh
+++ auto_seed.sh
@@ -3,17 +3,25 @@
 # Configurazione
 WATCH_DIR="/path/to/torrents"  # Directory da monitorare
 TORRENT_OUTPUT_DIR="/path/to/torrents_created"  # Dove salvare i .torrent
+TRACKER_FILE="/path/to/trackers.txt"  # File contenente i tracker
 TRANSMISSION_HOST="localhost"  # Host di Transmission
 TRANSMISSION_PORT="9091"       # Porta di Transmission
 USERNAME="user"                # Username di Transmission (se configurato)
 PASSWORD="password"            # Password di Transmission (se configurata)
-TRACKER_URL="udp://tracker.opentrackr.org:1337/announce,udp://opentracker.i2p.rocks:6969/announce,udp://tracker.openbittorrent.com:6969/announce,udp://open.stealth.si:80/announce,udp://tracker.torrent.eu.org:451/announce,udp://tracker.moeking.me:6969/announce,udp://tracker.zerobytes.xyz:1337/announce,udp://tracker.dler.org:6969/announce,udp://tracker1.bt.moack.co.kr:80/announce,udp://tracker.cyberia.is:6969/announce" # Tracker per i .torrent
 
 # Creazione e aggiunta torrent
 create_and_add_torrent() {
     local target="$1"
     local torrent_name="$(basename "$target").torrent"
     local torrent_file="$TORRENT_OUTPUT_DIR/$torrent_name"
+
+    # Leggere i tracker dal file e unirli in una stringa separata da virgole
+    if [[ -f "$TRACKER_FILE" ]]; then
+        TRACKER_URL=$(tr '\n' ',' < "$TRACKER_FILE" | sed 's/,$//')  # Rimuove la virgola finale
+    else
+        echo "Errore: Il file dei tracker $TRACKER_FILE non esiste."
+        exit 1
+    fi
 
     # Creare il file .torrent
     mktorrent -a "$TRACKER_URL" -o "$torrent_file" "$target"
